5c5793e501f98f380f56c94de17ff0acddc34839
mthembuavuyile <mthembuavuyile@gmail.com>
2026-04-14 21:12:17 +0200

    feat: initialize project with Tailwind CSS and base HTML structure

--- makhaswa/tailwind.config.js
+++ makhaswa/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./services.html"],
   darkMode: "class",
   theme: {
       extend: {
